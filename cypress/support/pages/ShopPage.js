eeca5fe1a3db17c36de9b71760f1f9441d243822
bik0052 <42190410+bik0052@users.noreply.github.com>
2025-09-07 10:54:07 +1200

    Update ShopPage.js

--- cypress/support/pages/ShopPage.js
+++ cypress/support/pages/ShopPage.js
@@ -1,1 +1,40 @@
+import { BasePage } from './BasePage.js';
 
+export class ShopPage extends BasePage {
+  constructor() {
+    super();
+    this.cartLink = '#nav-cart';
+    
+    this.products = {
+      'Stuffed Frog': { 
+        buyButton: '.product:contains("Stuffed Frog") .btn-success' 
+      },
+      'Fluffy Bunny': { 
+        buyButton: '.product:contains("Fluffy Bunny") .btn-success' 
+      },
+      'Valentine Bear': { 
+        buyButton: '.product:contains("Valentine Bear") .btn-success' 
+      }
+    };
+  }
+
+  buyProduct(productName, quantity) {
+    cy.logStep(`Purchasing ${quantity}x ${productName}`);
+    
+    for (let i = 0; i < quantity; i++) {
+      cy.contains('.product', productName)
+        .find('.btn-success, .btn')
+        .first()
+        .click();
+      cy.wait(500);
+    }
+    return this;
+  }
+
+  goToCart() {
+    cy.logStep('Navigating to cart');
+    this.clickElement(this.cartLink);
+    cy.url().should('contain', 'cart');
+    return this;
+  }
+}
